038522d30645d58bbe72fdf51539386716a444aa
92pino <jjb8382@gmail.com>
2020-06-15 22:44:04 +0900

    🍀 This-is-Swift Study
    - Closure
    - Optional Chaining

--- AccessControl.playground/Contents.swift
+++ AccessControl.playground/Contents.swift
@@ -73,3 +73,46 @@ private class AClass {
   }
 }
 
+// AClass의 접근 수준이 비공개 접근수준이므로 공개 접근수준 함수의 매개변수나 반환값으로 사용할 수 없다.
+//public func someFunction(a: AClass) -> AClass {
+//  return a
+//}
+
+
+// private과 filePrivate의 차이
+public struct SomeType {
+  private var privateVariable = 0
+  fileprivate var fileprivateVariable = 0
+}
+
+extension SomeType {
+  public func publicMethod() {
+    print("\(self.privateVariable), \(self.fileprivateVariable)")
+  }
+  
+  private func privateMethod() {
+    print("\(self.privateVariable), \(self.fileprivateVariable)")
+  }
+  
+  fileprivate func fileprivateMethod() {
+    print("\(self.privateVariable), \(self.fileprivateVariable)")
+  }
+}
+
+struct AnotherType {
+  var someInstance: SomeType = SomeType()
+  
+  mutating func someMethod() {
+    self.someInstance.publicMethod()
+    
+    self.someInstance.fileprivateVariable = 100
+    self.someInstance.fileprivateMethod()
+    
+    // 다른 타입 내부의 코드이므로 private 요소에 접근 불가
+    // self.someInstance.privateVariable = 100
+    // self.someInstance.privateMethod()
+  }
+}
+
+var anotherInstance = AnotherType()
+anotherInstance.someMethod()
