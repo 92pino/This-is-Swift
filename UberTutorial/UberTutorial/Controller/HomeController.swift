c5256a897d6f2cf971879baa4781b38f133e83e6
92pino <jjb8382@gmail.com>
2020-06-24 03:04:38 +0900

    🌈 Uber Clone Project
    - Custom TableView Cell add

--- UberTutorial/UberTutorial/Controller/HomeController.swift
+++ UberTutorial/UberTutorial/Controller/HomeController.swift
@@ -115,6 +115,8 @@ class HomeController: UIViewController {
     
     tableView.register(LocationCell.self, forCellReuseIdentifier: reuseIdentifier)
     tableView.rowHeight = 60
+    // 테이블 하단에 빈 공간 추가하기
+    tableView.tableFooterView = UIView()
     
     let height = view.frame.height - locationInputViewHeight
     tableView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: height)
@@ -169,11 +171,11 @@ extension HomeController: LocationInputActivationViewDelegate {
 
 extension HomeController: LocationInputViewDelegate {
   func dismissLocationInputView() {
-    locationInputView.removeFromSuperview()
     UIView.animate(withDuration: 0.3, animations: {
       self.locationInputView.alpha = 0
       self.tableView.frame.origin.y = self.view.frame.height
     }) { _ in
+      self.locationInputView.removeFromSuperview()
       UIView.animate(withDuration: 0.3) {
         self.inputActivationView.alpha = 1
       }
@@ -181,9 +183,19 @@ extension HomeController: LocationInputViewDelegate {
   }
 }
 
+// MARK: - UITableViewDelegate/DataSource
+
 extension HomeController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Test"
+  }
+  
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return section == 0 ? 2 : 5
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- UberTutorial/UberTutorial/View/LocationCell.swift
+++ UberTutorial/UberTutorial/View/LocationCell.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class LocationCell: UITableViewCell {
 
   // Mark: - Properties
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 14)
+    label.text = "123 Main Street"
+    
+    return label
+  }()
+  
+  private let addressLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 14)
+    label.textColor = .lightGray
+    label.text = "123 Main Street, Washington, DC"
+    
+    return label
+  }()
   
   // Mark: - Lifecycle
   
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+    selectionStyle = .none
+    
+    let stack = UIStackView(arrangedSubviews: [titleLabel, addressLabel])
+    stack.axis = .vertical
+    stack.distribution = .fillEqually
+    stack.spacing = 4
+    
+    addSubview(stack)
+    stack.snp.makeConstraints {
+      $0.centerY.equalTo(self.snp.centerY)
+      $0.left.equalToSuperview().offset(12)
+    }
   }
   
   required init?(coder: NSCoder) {
