c1943cdf8a70b3d302a5294cc5b6dbf31cafe937
92pino <jjb8382@gmail.com>
2020-09-28 00:26:58 +0900

    Chore : FCM Push Notifications

--- FCM_Test/FCM_Test/AppDelegate.swift
+++ FCM_Test/FCM_Test/AppDelegate.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseCore
+import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,27 +17,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
-    // Firebase 초기 셋팅
-    FirebaseApp.configure()
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+      // 사용할 UNAuthorizationOptions 옵션들
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
     
-    Messaging.messaging().delegate = self
-    UNUserNotificationCenter.current().delegate = self
-    // 사용할 UNAuthorizationOptions 옵션들
-    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-    
-    UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
+      // 권한 요청
+      UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
+    } else {
+      let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
     
     // APNS 등록
     application.registerForRemoteNotifications()
     
+    // Firebase 초기 셋팅
+    FirebaseApp.configure()
+    
+    Messaging.messaging().delegate = self
+    
     return true
   }
   
+  
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    let deviceTokenString = deviceToken.reduce("", { $0 + String(format: "%02X", $1) })
     
-    dump("\n---------- [ Log : deviceToken - \(deviceTokenString) ] ----------\n")
+    
+    if #available(iOS 13.0, *) {
+      // iOS 13 환경에서 Base16-encoded / hexadecimal string으로 표현
+      let deviceTokenString = deviceToken.reduce("", { $0 + String(format: "%02X", $1) })
+      dump("\n---------- [ Log : deviceToken - \(deviceTokenString) ] ----------\n")
+    } else {
+      // iOS 12 환경에서는 deviceToken 데이터를 String으로 가공
+      var deviceTokenString = String(format: "%@", deviceToken as CVarArg)
+      deviceTokenString = deviceTokenString.replacingOccurrences(of: "<", with: "")
+      dump("\n---------- [ Log : deviceToken - \(deviceTokenString) ] ----------\n")
+    }
+    
     Messaging.messaging().apnsToken = deviceToken
+    
+    // 앱을 다시 시작해도 설정 값을 유지하고 싶을 경우
+    // Messaging.messaging().isAutoInitEnabled = true
   }
 
 
@@ -55,7 +77,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 }
 
 extension AppDelegate: MessagingDelegate {
-  // MARK: - didReceiveRegistrationToken : 토큰 갱신을 모니터링
+  // MARK: - didReceiveRegistrationToken : 토큰 갱신을 모니터링  (토큰이 업데이트될 때마다 알림 받기 위한 메서드)
   /*
    토큰이 갱신되는 경우
    - 새 기기에서 앱을 복원할 경우
