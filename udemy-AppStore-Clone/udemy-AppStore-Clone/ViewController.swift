6ef2439049ea494607c3bf23a1cba0fde61ec068
92pino <jjb8382@gmail.com>
2020-11-25 23:53:21 +0900

    AppStore Clone Project init

---
+++ udemy-AppStore-Clone/udemy-AppStore-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udemy-AppStore-Clone
+//
+//  Created by JinBae Jeong on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
--- 꼼꼼한 재은씨 실전편/Chapter04/Chapter04-SideBarDIY/Chapter04-SideBarDIY/SideBarViewController.swift
+++ 꼼꼼한 재은씨 실전편/Chapter04/Chapter04-SideBarDIY/Chapter04-SideBarDIY/SideBarViewController.swift
@@ -8,82 +8,52 @@
 import UIKit
 
 class SideBarViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+  
+  let titles = [
+    "메뉴 01",
+    "메뉴 02",
+    "메뉴 03",
+    "메뉴 04",
+    "메뉴 05"
+  ]
+  
+  let icons = [
+    UIImage(named: "icon01.png"),
+    UIImage(named: "icon02.png"),
+    UIImage(named: "icon03.png"),
+    UIImage(named: "icon04.png"),
+    UIImage(named: "icon05.png")
+  ]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 0
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return self.titles.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let id = "menucell"
+    var cell = tableView.dequeueReusableCell(withIdentifier: id)
+    
+    if cell == nil {
+      cell = UITableViewCell(style: .default, reuseIdentifier: id)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    cell?.textLabel?.text = titles[indexPath.row]
+    cell?.imageView?.image = icons[indexPath.row]
+    cell?.textLabel?.font = UIFont.systemFont(ofSize: 14)
+    
+    return cell!
+  }
+  
 }
